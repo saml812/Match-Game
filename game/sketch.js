e0225b398466da2df39a4d6f1a4486cf07886124
Sam Lin <slbusi2055@gmail.com>
2024-08-30 11:48:55 -0400

    Added Card class and global functions

--- game/sketch.js
+++ game/sketch.js
@@ -45,15 +45,15 @@ function draw() {
       stroke('magenta');
       strokeWeight(5)
       line(finger.x, thumb.x, finger.y, thumb.y)
-      
-      if (length <= 15) {
+
+      // if (length <= 15) {
         
-      }
+      // }
     }
 
   }
   fill(0, 0, 255, 127);
-  rect(150, 150, 100, 200);
+  rect(100, 100, 100, 150);
 }
 
 // Callback function for when handPose outputs data
@@ -61,3 +61,80 @@ function gotHands(results) {
   // save the output to the hands variable
   hands = results;
 }
+
+// Class Object: Cards
+// Global functions
+
+class Card {
+  constructor(key, pos, size=[100,100]) {
+      this.key = key;
+      this.pos = pos
+      this.size = size
+      this.text = "";
+      this.color = (255,0,0);
+      this.isFlipped = true;
+      this.isMatched = false;
+  }
+
+  //getter
+  get isFlipped(){
+    return this.isFlipped;
+  }
+
+  get isMatched(){
+    return this.isMatched;
+  }
+
+  get key(){
+    return this.key;
+  }
+
+  //setter
+  set isFlipped(bool){
+    this.isFlipped = bool;
+  }
+
+  set text(string){
+    this.text = string;
+  }
+
+  set color(color){
+    this.color = color;
+  }
+
+  set isMatched(bool){
+    this.isMatched = bool;
+  }
+}
+
+function flipCard(Card){
+  if (Card.flipState){
+    Card.flipState(false);
+    Card.text("");
+    Card.color("blue");
+  } else {
+    Card.flipState(true);
+    Card.text(Card.key);
+    Card.color("red");
+  }
+}
+
+function compare(Card1, Card2){
+  return Card1.key == Card2.key;
+}
+
+function match(Card1, Card2){
+  if (compare(Card1, Card2)){
+    Card1.isMatched(true);
+    Card2.isMatched(true);
+    Card1.color("green");
+    Card2.color("green");
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
+
