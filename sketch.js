cfd0516f89942a81ee215267d9e9fbf97e8441f7
Sam Lin <slbusi2055@gmail.com>
2024-09-07 01:07:37 -0400

    Changed handpose model to mediapipe, matching based on colors, matched cards disappear from webcam

--- sketch.js
+++ sketch.js
@@ -1,81 +1,108 @@
-let handPose;
+let handsModel;
 let video;
-let hands = [];
-
-const options = {
-    maxHands: 1,
-    flipped: true,
-    runtime: "tfjs",
-    modelType: "full",
-}
-
-function preload() {
-  handPose = ml5.handPose(options);
-}
-
-function setup() {
-  createCanvas(640, 480);
-  video = createCapture(VIDEO, { flipped: true });
-  video.size(640, 480);
-  video.hide();
-  handPose.detectStart(video, gotHands);
-
-  initializeCards();
-}
-
+let cards = [];
 let a = null;
 let b = null;
 let counter = 0;
 let allowClick = true;
+let score = 0;
+let gameOver = false;
+let finger = null;
+let thumb = null;
+
+const canvasWidth = 800;
+const canvasHeight = 600;
+
+function setup() {
+  createCanvas(canvasWidth, canvasHeight);
+  video = createCapture(VIDEO, { flipped: true });
+  video.size(canvasWidth, canvasHeight);
+  video.hide();
+  initializeCards();
+  textAlign(CENTER, CENTER);
+  
+  // Set up Mediapipe Hands
+  handsModel = new Hands({locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+  }});
+  
+  handsModel.setOptions({
+    maxNumHands: 1,
+    modelComplexity: 1,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5
+  });
+  
+  handsModel.onResults(onResults);
+
+  const camera = new Camera(video.elt, {
+    onFrame: async () => {
+      await handsModel.send({ image: video.elt });
+    },
+    width: canvasWidth,
+    height: canvasHeight
+  });
+  camera.start();
+}
 
 function draw() {
   image(video, 0, 0, width, height);
+
+  // if (gameOver) {
+  //   displayVictory();
+  //   return;
+  // }
 
   for (let card of cards) {
     card.show();
   }
 
-  if (hands.length > 0 && allowClick) {
-    let finger = hands[0].index_finger_tip;
-    let thumb = hands[0].thumb_tip;
+  fill(255);
+  textSize(50);
+  text(`Score: ${score}`, 100, canvasHeight - 20);
 
+  if (finger && thumb && allowClick) {
     fill(0, 255, 0);
     noStroke();
     circle(finger.x, finger.y, 10);
     circle(thumb.x, thumb.y, 10);
 
     for (let card of cards) {
-      if (card.isHovered(finger)) {
+      if (card.isHovered(finger) && !card.isMatched) {
+        card.highlight();
         let length = dist(finger.x, finger.y, thumb.x, thumb.y);
 
         if (card.isFlipped || card.isMatched) {
           console.log("cannot click");
         } else {
           console.log("clickable");
-          if (length < 20 && !card.isFlipped && !card.isMatched) {
-            flipCard(card);
-            counter += 1;
+        }
+        
+        if (!card.isFlipped && !card.isMatched && length < 20) {
+          flipCard(card);
+          counter += 1;
 
-            if (counter == 1) {
-              a = card;
-            } else if (counter == 2) {
-              b = card;
-              allowClick = false;  // Disable clicking
+          if (counter == 1) {
+            a = card;
+          } else if (counter == 2) {
+            b = card;
+            allowClick = false;
 
-              setTimeout(() => {
-                if (compare(a, b)) {
-                  console.log("matched");
-                  a.isMatched = true;
-                  b.isMatched = true;
-                } else {
-                  console.log("no match!");
-                  flipCard(a);
-                  flipCard(b);
-                }
-                counter = 0;
-                allowClick = true;
-              }, 1000);  // 1 second delay
-            }
+            setTimeout(() => {
+              if (compare(a, b)) {
+                console.log("matched");
+                a.isMatched = true;
+                b.isMatched = true;
+                score++;
+                //checkVictory();
+              } else {
+                console.log("no match!");
+                flipCard(a);
+                flipCard(b);
+              }
+              counter = 0;
+              allowClick = true;
+            }, 1000);  // 1 second delay
           }
         }
       }
@@ -83,13 +110,20 @@ function draw() {
   }
 }
 
-function gotHands(results) {
-  hands = results;
+function onResults(results) {
+  if (results.multiHandLandmarks && results.multiHandLandmarks.length > 0) {
+    const landmarks = results.multiHandLandmarks[0];
+    finger = { x: width - landmarks[8].x * width, y: landmarks[8].y * height };  // Index finger tip
+    thumb = { x: width - landmarks[4].x * width, y: landmarks[4].y * height };  // Thumb tip
+  } else {
+    finger = null;
+    thumb = null;
+  }
 }
 
 class Card {
-  constructor(pos, key, size = [75, 75]) {
-    this.key = key;
+  constructor(pos, size, color) {
+    this.color = color;
     this.pos = pos;
     this.size = size;
     this.isFlipped = false;
@@ -97,48 +131,47 @@ class Card {
   }
 
   show() {
-    let x = this.pos[0];
-    let y = this.pos[1];
-    let w = this.size[0];
-    let h = this.size[1];
-
     if (this.isMatched) {
-      fill(0, 255, 0);
+      fill(0,0,0,0);
     } else if (this.isFlipped) {
-      fill(255, 0, 0);
+      fill(this.color);
     } else {
-      fill(0, 0, 255);
+      fill("white");
     }
-    rect(x, y, w, h);
-
-    if (this.isFlipped || this.isMatched) {
-      fill(255);
-      textSize(32);
-      textAlign(CENTER, CENTER);
-      text(this.key, x + w / 2, y + h / 2);
-    }
+    rect(this.pos[0], this.pos[1], this.size[0], this.size[1]);
   }
 
   isHovered(finger) {
-    let x = this.pos[0];
-    let y = this.pos[1];
-    let w = this.size[0];
-    let h = this.size[1];
-    return finger.x > x && finger.x < x + w && finger.y > y && finger.y < y + h;
+    return finger.x > this.pos[0] && finger.x < this.pos[0] + this.size[0] && finger.y > this.pos[1] && finger.y < this.pos[1] + this.size[1];
+  }
+
+  highlight() {
+    stroke(255, 255, 0);
+    strokeWeight(4);
+    noFill();
+    rect(this.pos[0], this.pos[1], this.size[0], this.size[1]);
+    noStroke();
   }
 }
 
-let cards = [];
-
 function initializeCards() {
-  let keys = ["A", "B", "C", "D", "A", "B", "C", "D"];
-  keys = shuffle(keys);
+  let colors = ["green", "blue", "red", "purple", "green", "blue", "red", "purple"];
+  colors = shuffle(colors);
+
+  let rows = 2;
+  let cols = 4;
+  let cardWidth = 100;
+  let cardHeight = 100;
+  let gridWidth = cols * cardWidth + (cols - 1) * 25;
+  let gridHeight = rows * cardHeight + (rows - 1) * 25;
+  let x = (canvasWidth - gridWidth) / 2;
+  let y = (canvasHeight - gridHeight) / 2;
 
   let index = 0;
-  for (let i = 0; i < 2; i++) {
-    for (let j = 0; j < 4; j++) {
-      let pos = [j * 100 + 25, i * 100 + 25];
-      cards.push(new Card(pos, keys[index]));
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      let pos = [x + j * (cardWidth + 25), y + i * (cardHeight + 25)];
+      cards.push(new Card(pos, [cardWidth, cardHeight], colors[index]));
       index++;
     }
   }
@@ -149,5 +182,18 @@ function flipCard(card) {
 }
 
 function compare(a, b) {
-  return a.key == b.key;
+  return a.color == b.color;
 }
+
+// function checkVictory() {
+//   if (score == cards.length / 2) {
+//     gameOver = true;
+//   }
+// }
+
+// function displayVictory() {
+//   fill(0, 255, 0);
+//   textSize(200);
+//   text("You Win!", width / 2, height / 2);
+//   noLoop();
+// }
